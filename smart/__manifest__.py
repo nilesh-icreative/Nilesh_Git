174690f13fc0bc5f7a809bb454e85494085ceb1c
odoo <nilesh@icreativetechnolabs.com>
2022-03-08 18:21:19 +0530

    [name_get_search_default_get_messag_paot] smart

--- smart/__manifest__.py
+++ smart/__manifest__.py
@@ -20,13 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail','sale'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/related.xml',
+        'views/saleview.xml',
+
 
     ],
     # only loaded in demonstration mode
