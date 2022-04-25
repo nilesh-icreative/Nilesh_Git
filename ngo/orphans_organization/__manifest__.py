1a7e191dc3f5de2f6a983c3ec4a6ba39d17fba5a
odoo <nilesh@icreativetechnolabs.com>
2022-04-25 18:14:32 +0530

    [css_apply]

--- ngo/orphans_organization/__manifest__.py
+++ ngo/orphans_organization/__manifest__.py
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Nilesh Vaghela",
+    'website': "http://www.aktivsoftware.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Ngo',
+    'version': '15.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/expense_type.xml',
         'views/ngo_bool.xml',
         'views/views.xml',
         'views/wizards.xml',
