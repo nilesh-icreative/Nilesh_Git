7c266e9844e1374d370d9b1d50797085c3521bb4
odoo <nilesh@icreativetechnolabs.com>
2022-05-30 18:19:00 +0530

    [Edit_record_frontend]

--- v_15_project/controller_task/controllers/login_form.py
+++ v_15_project/controller_task/controllers/login_form.py
@@ -9,8 +9,27 @@ class Login(http.Controller):
     def login_forms(self, **kw):
         return http.request.render('controller_task.login_forms_page', {})
 
-    @http.route('/create/contacts', type="http", auth="public", website=True)
+    @http.route(["/login_forms/<model('res.partner'):con>/"], type="http",
+                auth='public', website=True)
+    def person_details(self, con, **kw):
+        return http.request.render('controller_task.login_forms_page', {
+            'data': con,
+        })
+
+    @http.route("/create/contacts", type="http",
+                auth="public", website=True)
     def create_contacts(self, **kw):
-        request.env['res.partner'].sudo().create(kw)
-        return request.render(
-            'controller_task.contacts_create_successfully', {})
+
+        if kw.get('contact_id'):
+            data = request.env['res.partner'].sudo().search([('id', '=', kw.get('contact_id'))])
+            data.write({
+                'name': kw.get('name'),
+                'phone': kw.get('phone'),
+                'email': kw.get('email'),
+            })
+            return request.redirect('/contacts_details')
+        else:
+            request.env['res.partner'].sudo().create(kw)
+            return request.render(
+                'controller_task.contacts_create_successfully', {}
+            )
