7cac7e2f1a7c82c0313a079670f8590f0730f216
odoo <nilesh@icreativetechnolabs.com>
2022-06-06 15:20:57 +0530

    [Frontend_image_upload]

--- v_15_project/frontend_paper/controllers/create_contacts.py
+++ v_15_project/frontend_paper/controllers/create_contacts.py
@@ -1,26 +1,26 @@
 from odoo import http
 from odoo.http import request
-
+import base64
 
 class ContactForm(http.Controller):
 
     @http.route("/create/contact", type="http", auth="public", website=True)
     def create_contacts(self, **kw):
-
         if kw:
-            contact_record = {
-                'name': kw.get('name'),
-                'email': kw.get('email'),
-                'phone': kw.get('phone'),
-            }
-
-            request.env['res.partner'].sudo().create(contact_record)
+            new_files = request.httprequest.files.getlist('image_1920')
+            for file in new_files:
+                partner = request.env['res.partner'].create({
+                    'name': kw.get('name'),
+                    'email': kw.get('email'),
+                    'phone': kw.get('phone'),
+                    'image_1920': base64.b64encode(file.read())
+                })
             return request.render(
-                'frontend_paper.contacts_create_successfully', {}
+                    'frontend_paper.contacts_create_successfully', {}
             )
         else:
             return request.render(
-                'frontend_paper.create_contacts_form_view', {}
+                'frontend_paper.create_contacts_form_view_id', {}
             )
 
     @http.route(['/contacts_details'], type='http',
